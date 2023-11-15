567935ddbdb414a23c6a71dceef483e3fddf0297
yunchi <13524919136@163.com>
2023-11-14 22:57:23 -0500

    dummy trace

---
+++ data/memcached/gen_dummy.py
@@ -0,0 +1,35 @@
+import random
+import pandas as pd
+
+sample_trace = "memcached-sample"
+num_tenants = 4
+time_span = 20
+
+
+df = pd.read_csv(sample_trace, nrows=100,
+                 names=["ts", "key", "key_size", "val_size", "client_id", "op", "ttl"])
+df.drop("key_size", axis=1, inplace=True)
+df.sort_values(by="client_id", inplace=True)
+df.reset_index(inplace=True, drop=True)
+
+bin_edges = []
+bin_size = len(df) // num_tenants
+for i in range(num_tenants):
+    ii = min(len(df), (i+1)*bin_size)-1
+    bin_edges.append((df.loc[ii, "client_id"], i))
+
+for i in range(len(df)):
+    new_client_id = 0
+    for bin_edge, cid in bin_edges:
+        if df.loc[i, "client_id"] <= bin_edge:
+            new_client_id = cid
+            break
+    df.loc[i, "client_id"] = new_client_id
+    df.loc[i, "ts"] = int(random.uniform(0, time_span))
+    df.loc[i, "op"] = "get" if df.loc[i, "op"] in ["get", "gets"] else "set"
+
+df.rename(columns={'client_id': 'tenant_id'}, inplace=True)
+df.sort_values(by="ts", inplace=True)
+df.reset_index(inplace=True, drop=True)
+
+df.to_csv(f"dummy_q{len(df)}_d{time_span}_t{num_tenants}.csv", index=False)
